50f537d828bba486a6ad433c4dad9c40f14b42ca
Gustavo Faria <gustavoofaria@outlook.com>
2023-04-24 10:58:32 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/batch/Livro.java
@@ -0,0 +1,23 @@
+package br.com.fiap.batch;
+
+public class Livro {
+
+    private String titulo;
+    private int quantidade;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
